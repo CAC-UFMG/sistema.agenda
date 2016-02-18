2b39295d9fa67c497d29d18530ae5691c3bbb3c7
cacufmg <quilombo@gmail.com>
2016-02-18 19:24:28 -0200

    correcao de workflow

--- sistema/agenda/Extensions/moveItem.py
+++ sistema/agenda/Extensions/moveItem.py
@@ -1,7 +1,7 @@
 from Acquisition import aq_inner, aq_parent
 
 def copiaParaPastaMae(self, state_change):
-   pai = aq_parent(aq_inner(self))
-   clipboard = pai.manage_cutObjects([self.id])
+   pai = aq_parent(aq_inner(state_change.object))
+   clipboard = pai.manage_cutObjects([state_change.object.id])
    dest = aq_parent(aq_inner(pai))
    result = dest.manage_pasteObjects(clipboard)
