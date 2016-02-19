788845116a31d3cab35dc8287c7921dfca863127
cacufmg <quilombo@gmail.com>
2016-02-19 11:55:32 -0200

    Atualizando o calendario

--- sistema/agenda/Extensions/moveItem.py
+++ sistema/agenda/Extensions/moveItem.py
@@ -1,7 +1,13 @@
 from Acquisition import aq_inner, aq_parent
+from zope.lifecycleevent import modified
 
-def copiaParaPastaMae(self, state_change):
+def copiaParaPastaMae(self, state_change):   
    pai = aq_parent(aq_inner(state_change.object))
    clipboard = pai.manage_cutObjects([state_change.object.id])
    dest = aq_parent(aq_inner(pai))
    result = dest.manage_pasteObjects(clipboard)
+   
+def reindexa(self, state_change):      
+   obj = state_change.object
+   modified(obj)
+   #obj.reindexObject(idxs=["review_state"])
