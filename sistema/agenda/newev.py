03a0ea8ce0477461029af3f442cc8fb65705ec95
cacufmg <quilombo@gmail.com>
2016-02-23 14:56:47 -0300

    Correcoes

--- sistema/agenda/newev.py
+++ sistema/agenda/newev.py
@@ -4,7 +4,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class newEv(BrowserView):
     """ Render the title and description of item only (example)
     """
-    index = ViewPageTemplateFile("evento_templates/newev.pt")
+    index = ViewPageTemplateFile("resources/newev.pt")
 	
     def render(self):
         return self.index()
